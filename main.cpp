047d8f21d0e00802e20080f4861fe14cf814e53d
cesar214567 <cesar.madera@utec.edu.pe>
2019-11-04 15:27:36 -0500

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream> 
+#include <vector>
+#include <map>
+#include <stdio.h>
+#include <sstream>
 using namespace std;
 
 int main(){
-
+    map<string,string> diccionario;
+    string key,val, str;
+    stringstream ss;
+    while(getline(cin, str) && str.size()){
+        ss.clear();
+        ss << str;
+        ss >> val;
+        ss >> key;
+        diccionario[key] = val;
+    }
+    string palabra;
+    while (cin>>palabra ){
+        auto it =diccionario.find(palabra);
+        if (it!=diccionario.end()){
+            cout<<it->second<<endl;
+        }else{
+            cout<<"eh"<<endl;
+        }
+    }
 }
